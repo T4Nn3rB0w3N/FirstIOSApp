171a133604011d2049e83e2351de6a36fa88e1c7
Bowen <tbow1382@740s-pl-120.local>
2018-10-09 08:44:49 -0600

    created new folder for color tool, created a button

---
+++ FirstIOSApp/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSApp
+//
+//  Created by Bowen, Tanner on 10/9/18.
+//  Copyright © 2018 CSP. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
